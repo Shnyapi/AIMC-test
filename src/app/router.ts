e1c902644f2e81aec97bda6bf65ce694b8084e48
Shnyapi <mrur3@mail.ru>
2019-01-25 03:20:06 +0500

    add preload stategy

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,18 +1,24 @@
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadingStrategy, Route } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
-// const childrenRoutes: Routes = [
-//   { path: 'form', loadChildren: './general-module/general.module#GeneralModule', data: { preload: false } },
-// ];
+// Обработчик предзагрузки модулей
+class AppCustomPreloader implements PreloadingStrategy {
+  preload(route: Route, load: Function): Observable<any> {
+    return route.data && route.data.preload ? load() : of(null);
+  }
+}
 
 const routes: Routes = [
   { path: '', redirectTo: 'form', pathMatch: 'full' },
-  { path: 'form', loadChildren: './form-module/form.module#FormModule' },
+  { path: 'form', loadChildren: './form-module/form.module#FormModule', data: {preload: true} },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: AppCustomPreloader
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
